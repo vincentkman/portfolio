1ce328c7e56a6938d36bdc412c8fdd805624487b
Vincent Man <vincentman2702@hotmail.com>
2020-02-15 15:40:11 +0000

    update about page

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -12,7 +12,12 @@ function About() {
             <div className='about-container'>
                 <h1 className='title'>About me</h1>
                 <Jumbotron>
-                    <p>Hi, my name is Vincent and I'm a self-taught Front-End Developer and work in Accounting based in London, UK. I enjoy solving real-world problems and turning them into simple, mobile-first ready, fully responsive and beautiful designs. As I code every single day, my passion for web development continues to grow massively. After putting a lot of thought about my future career, I made a big decision and chose to follow my passion in Web Development. I started learning HTML5, CSS3, and JavaScript using the free resources from Free Code Camp, Coursera. When I finally became serious in this field, I immediately signed up for Treehouse Front End Development Techdegree. Learning from the online resources significantly boosted my coding skills and gave me a good foundation for building first-class websites as a Front-End Developer. When I am not coding or trying to position objects correctly, I usually cook, go to the gym or out photographing.
+                    <p>
+                        Hi, my name is Vincent and I'm a Software Developer and currently teaching new students in coding based in London, UK. I enjoy solving real-world problems and turning them into simple, mobile-first ready, fully responsive and beautiful designs. As I code every single day, my passion for web development continues to grow massively. After putting a lot of thought about my future career, I made a big decision and chose to follow my passion in Web Development. I started learning HTML5, CSS3, and JavaScript using the free resources from Free Code Camp, Coursera. When I finally became serious in this field, I immediately signed up for Treehouse Front End Development Techdegree. Learning from the online resources significantly boosted my coding skills and gave me a good foundation for building first-class websites as a Developer. 
+                        
+                        After becoming confident, I decided to step up my game and joined a coding bootcamp at Kodiri to learn more. Shortly after the end of the bootcamp, I was offered a role to become an assistant coding instructor.   
+                        
+                        When I am not coding or trying to position objects correctly, I usually cook, go to the gym or out photographing.
                         </p>
                 </Jumbotron>
             </div>
